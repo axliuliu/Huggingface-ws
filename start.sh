dcbd984f48393f3d6b0d8fec1781c82334e9a36a
axliuliu <9866liu@gmail.com>
2025-10-18 17:39:08 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-export NEZHA_SERVER="a.holoy.dpdns.org:36958"
+export NEZHA_SERVER="a.holoy.dpdns.org"
 export NEZHA_PORT="36958"  #当端口设置为443时，自动开启TLS,无需设置
 export NEZHA_KEY="NwxKJwM9UKRCX5TBPaBm0IrjNCSyflif"
 
