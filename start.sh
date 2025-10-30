00497ce93fdddbba774cf14c44eb213d98665537
axliuliu <9866liu@gmail.com>
2025-10-30 10:03:56 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
-export NEZHA_SERVER="a.holoy.dpdns.org"
+export NEZHA_SERVER="a.holoy.dpdns.org:36958"
 export NEZHA_PORT="36958"  #当端口设置为443时，自动开启TLS,无需设置
-export NEZHA_KEY="NwxKJwM9UKRCX5TBPaBm0IrjNCSyflif"
+export NEZHA_KEY="VHTEk5wXIwqMobTFE7wtJYy88ZtIRops"
 
 chmod +x server
 if [ "$NEZHA_PORT" = "443" ]; then
